e90263e878bf9ff6209b221fd7733fc9e9cb41ea
Mykola Kuchmynda <mykola.kuchmynda@gmail.com>
2025-03-23 11:55:02 +0200

    some changes

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -2,15 +2,6 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-iziToast.show({
-  title: "Готово!",
-  message: "iziToast підключено через npm",
-});
-flatpickr("#datetime-picker", {
-  enableTime: true,
-  time_24hr: true,
-  dateFormat: "Y-m-d H:i",
-});
 
 const datetimePicker = document.querySelector("#datetime-picker");
 const startButton = document.querySelector("[data-start]");
@@ -22,13 +13,16 @@ const secondsSpan = document.querySelector("[data-seconds]");
 let userSelectedDate = null;
 let countdownInterval = null;
 
+// Спочатку блокуємо кнопку "Start"
+startButton.disabled = true;
+
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        if (selectedDates[0] <= new Date()) {
+        if (selectedDates[0].getTime() <= Date.now()) {
             iziToast.error({ title: "Error", message: "Please choose a date in the future" });
             startButton.disabled = true;
         } else {
@@ -69,10 +63,15 @@ function startCountdown() {
     startButton.disabled = true;
     datetimePicker.disabled = true;
 
+    // Зупиняємо попередній таймер
+    if (countdownInterval) {
+        clearInterval(countdownInterval);
+    }
+
     countdownInterval = setInterval(() => {
         const currentTime = new Date();
         const timeDiff = userSelectedDate - currentTime;
-        
+
         if (timeDiff <= 0) {
             clearInterval(countdownInterval);
             updateTimerDisplay({ days: 0, hours: 0, minutes: 0, seconds: 0 });
