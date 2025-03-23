5184c4aeb3449c264cf20525f01a67610ebbc406
Mykola Kuchmynda <mykola.kuchmynda@gmail.com>
2025-03-23 10:32:18 +0200

    hw done

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,37 @@
+
+import iziToast from "izitoast";
+
+import "izitoast/dist/css/iziToast.min.css";
+document.querySelector(".form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    
+    const form = event.target;
+    const delay = parseInt(form.delay.value, 10);
+    const state = form.state.value;
+    
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === "fulfilled") {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    })
+    .then(delay => {
+        iziToast.success({
+            title: "Success",
+            message: `✅ Fulfilled promise in ${delay}ms`,
+            position: "topRight"
+        });
+    })
+    .catch(delay => {
+        iziToast.error({
+            title: "Error",
+            message: `❌ Rejected promise in ${delay}ms`,
+            position: "topRight"
+        });
+    });
+    
+    form.reset();
+});
